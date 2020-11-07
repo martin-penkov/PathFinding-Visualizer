bf8aabc60156348cee9562d8d9f30fc4b39459d2
Martin Penkov <59456450+martin-penkov@users.noreply.github.com>
2020-11-07 16:43:34 +0200

    refactor readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('exampletest', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //testing
+  //find elements via screen var; check
+  
 });
